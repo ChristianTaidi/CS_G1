71d1e7c251cba581ed6dc1ae385fa25479682f14
Aitana Cerezo Camino <43374601+aitanaceca@users.noreply.github.com>
2019-04-22 20:17:29 +0200

    update

--- app/src/test/Resources/scenarios.feature
+++ app/src/test/Resources/scenarios.feature
@@ -2,28 +2,27 @@ Feature: Play to Space invaders
   As a user
   I want play game
 
-  Scenario: Do login in the app
-    Given  We are a user
-    And  We enter to app
-    When we enter name and age
-    Then We start game
+  Scenario: Move space ship
+    Given I want to move the ship down
+    When We press down buttom
+    Then Space Ship move down
 
   Scenario: Move space ship
-    Given A new game of S.I
+    Given I want to move the ship to the left
     When  We press left buttom
     Then  Space Ship move left
 
   Scenario: Shot space ship
-    Given A new game of S.I
+    Given I want to shot
     When  We press shot buttom
     Then  Space Ship shot a laser
 
   Scenario: Move space ship up
-    Given A new game of S.I
+    Given I want to move the ship up
     When  We press up buttom
     Then  Space Ship move up
 
   Scenario: Collide with barrier
-    Given A new game of S.I
+    Given I want to collide with the barrier
     When  The ship collides with barrier
     Then  The barrier disapears
---
+++ app/src/test/java/cs/spaceinvaders/LoginTest.java
@@ -0,0 +1,93 @@
+package cs.spaceinvaders;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import cs.spaceinvaders.entity.SpaceShip;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import javax.naming.Context;
+
+
+public class LoginTest {
+   public Context context;
+   public SpaceShip spaceShip;
+   public Bitmap spaceShipBitmap;
+
+   @Given("^I want to move the ship down$")
+   public void iWantToMoveTheShipDown() {
+      context = mock(Context.class);
+      spaceShipBitmap = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(context.getResources(), R.drawable.spaceship), x/8, y/15, false);
+      this.spaceShip = new SpaceShip(context,20,20, );
+   }
+
+   @When("^We press down buttom$")
+   public void wePressDownButtom() {
+
+   }
+
+   @Then("^Space Ship move down$")
+   public void spaceShipMoveDown() {
+
+   }
+
+   @Given("^I want to move the ship to the left$")
+   public void iWantToMoveTheShipToTheLeft() {
+
+   }
+
+   @When("^We press left buttom$")
+   public void wePressLeftButtom() {
+
+   }
+
+   @Then("^Space Ship move left$")
+   public void spaceShipMoveLeft() {
+
+   }
+
+   @Given("^I want to shot$")
+   public void iWantToShot() {
+
+   }
+
+   @When("^We press shot buttom$")
+   public void wePressShotButtom() {
+
+   }
+
+   @Then("^Space Ship shot a laser$")
+   public void spaceShipShotALaser() {
+
+   }
+
+   @Given("^I want to move the ship up$")
+   public void iWantToMoveTheShipUp() {
+
+   }
+
+   @When("^We press up buttom$")
+   public void wePressUpButtom() {
+
+   }
+
+   @Then("^Space Ship move up$")
+   public void spaceShipMoveUp() {
+
+   }
+
+   @Given("^I want to collide with the barrier$")
+   public void iWantToCollideWithTheBarrier() {
+   }
+
+   @When("^The ship collides with barrier$")
+   public void theShipCollidesWithBarrier() {
+
+   }
+
+   @Then("^The barrier disapears$")
+   public void theBarrierDisapears() {
+
+   }
+}
